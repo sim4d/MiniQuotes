eca5933b3efd6e8c6b4728a001d2f11a5a5cea83
Simford Dong <sim4dong@gmail.com>
2025-06-25 13:18:47 +0800

    add k.jung

---
+++ pages/kjung/kjung.js
@@ -0,0 +1,83 @@
+// pages/kjung/kjung.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 复制链接到剪贴板
+   */
+  copyUrl: function (e) {
+    const url = e.currentTarget.dataset.url;
+    wx.setClipboardData({
+      data: url,
+      success: function () {
+        wx.showToast({
+          title: '链接已复制',
+          icon: 'success',
+          duration: 2000
+        });
+      }
+    });
+  }
+})
